b685ce05ef4aac0c894de05efb07d754cf57cf42
dhimanshubham440818 <dhimanshubham440818@gmail.com>
2023-12-11 10:29:29 +0530

    deploy time on netlify

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./component/Home"
 import ChatPage from "./component/ChatPage";
 import socketIO from "socket.io-client"
 import './App.css'
 
-const socket = socketIO.connect("http://localhost:4000")
+const socket = socketIO.connect('https://chatapp-backend-lg1t.onrender.com')
 function App() {
   return (
     <BrowserRouter>
-        <div>
-          <Routes>
-            <Route path="/" element={<Home socket={socket}/>}></Route>
-            <Route path="/chat" element={<ChatPage socket={socket}/>}></Route>
-          </Routes>
-    </div>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home socket={socket} />}></Route>
+          <Route path="/chat" element={<ChatPage socket={socket} />}></Route>
+        </Routes>
+      </div>
     </BrowserRouter>
-    
+
   );
 }
 
